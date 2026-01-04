LineageOS/android_device_google_gs201

properties: BP4A.251205.006
Change-Id: Idc001a37251a1145dda8ecbd82ced65d8f2014a2

device.mk
@@ -654,7 +654,7 @@ PRODUCT_PACKAGES += \
 
 # Properties
 TARGET_PRODUCT_PROP += device/google/gs201/product.prop
-TARGET_SYSTEM_PROP += device/google/gs201/system.prop
+TARGET_SYSTEM_EXT_PROP += device/google/gs201/system_ext.prop
 
 # Tethering
 PRODUCT_PACKAGES += \

DerpFest-Devices/device_google_zuma

device.mk
@@ -142,7 +142,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.vendor.usb.displayport.enabled=1
 
 # Enable Settings 2-pane optimization for devices supporting display ports.
-PRODUCT_SYSTEM_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
         persist.settings.large_screen_opt_for_dp.enabled=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -658,7 +658,7 @@ PRODUCT_PACKAGES += \
 
 # Properties
 TARGET_PRODUCT_PROP += device/google/zuma/product.prop
-TARGET_SYSTEM_PROP += device/google/zuma/system.prop
+TARGET_SYSTEM_EXT_PROP += device/google/zuma/system_ext.prop
 
 # Tethering
 PRODUCT_PACKAGES += \